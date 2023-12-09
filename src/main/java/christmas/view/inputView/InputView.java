75c90d9c3bf04f0a46d6f2cabab21f2f47da1a64
jdohun <wjdehgns97@naver.com>
2023-12-09 23:18:01 +0900

    feat: 예상 방문 날짜 및 주문을 입력받는 기능 구현
    InputView.java

---
+++ src/main/java/christmas/view/inputView/InputView.java
@@ -0,0 +1,36 @@
+package christmas.view.inputView;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.view.inputView.inputValidator.InputValidator;
+
+public final class InputView {
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return Holder.INPUT_VIEW;
+    }
+
+    public String inputExpectedVisitDay() {
+        System.out.println(InputMessage.INPUT_EXPECTED_VISIT_DAY);
+        String expectedVisitDay = Console.readLine();
+        validateInput(expectedVisitDay);
+        return expectedVisitDay;
+    }
+
+    public String inputOrder() {
+        System.out.println(InputMessage.INPUT_ORDER);
+        String order = Console.readLine();
+        validateInput(order);
+        return order;
+    }
+
+    private void validateInput(String input) {
+        InputValidator.isNotNull(input);
+        InputValidator.isNotEmpty(input);
+    }
+
+    class Holder {
+        private static final InputView INPUT_VIEW = new InputView();
+    }
+}
