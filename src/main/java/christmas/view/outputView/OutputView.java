7f2239b07c713a7fd449121e8612f06155ec46f3
DH Jeong <wjdehgns97@gmail.com>
2023-12-09 23:16:52 +0900

    feat: 인사 메시지 출력 기능 구현
    OutputView.java

---
+++ src/main/java/christmas/view/outputView/OutputView.java
@@ -0,0 +1,20 @@
+package christmas.view.outputView;
+
+import static christmas.view.outputView.OutputMessage.GREETINGS;
+
+public final class OutputView {
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        return Holder.OUTPUT_VIEW;
+    }
+
+    public void showGreetings() {
+        System.out.println(GREETINGS.getMessage());
+    }
+
+    private class Holder {
+        private static final OutputView OUTPUT_VIEW = new OutputView();
+    }
+}
