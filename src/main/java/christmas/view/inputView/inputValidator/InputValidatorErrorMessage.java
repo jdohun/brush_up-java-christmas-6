6737449e40918ae8242fab127daf0c88497c5339
jdohun <wjdehgns97@naver.com>
2023-12-09 23:11:48 +0900

    feat: 입력 값 검증 기능 구현
    InputValidator.java
    InputValidatorErrorMessage.java

---
+++ src/main/java/christmas/view/inputView/inputValidator/InputValidatorErrorMessage.java
@@ -0,0 +1,18 @@
+package christmas.view.inputView.inputValidator;
+
+import static christmas.util.ErrorMessagePrefix.ERROR_MESSAGE_PREFIX;
+
+enum InputValidatorErrorMessage {
+    ERROR_NULL("null 을 입력할 수 없습니다."),
+    ERROR_EMPTY("빈 문자열을 입력할 수 없습니다.");
+    
+    private final String message;
+
+    InputValidatorErrorMessage(String message) {
+        this.message = ERROR_MESSAGE_PREFIX.getErrorMessagePrefix() + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
