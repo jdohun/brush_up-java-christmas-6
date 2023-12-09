2e5583aeba89a9a26320d08e4f95c4124a017ea0
DH Jeong <wjdehgns97@gmail.com>
2023-12-09 23:49:57 +0900

    feat: 예상 방문 날짜를 검증 후 저장하는 객체 구현
    ExpectedVisitDay.java
    ExpectedVisitDayErrorMessage.java

---
+++ src/main/java/christmas/domain/model/classes/expectedVisitDay/ExpectedVisitDayErrorMessage.java
@@ -0,0 +1,21 @@
+package christmas.domain.model.classes.expectedVisitDay;
+
+import static christmas.util.ErrorMessagePrefix.ERROR_MESSAGE_PREFIX;
+
+public enum ExpectedVisitDayErrorMessage {
+    /**
+     * 방문할 날짜에 숫자가 입력되지 않은 경우
+     * 1 이상 31 이하의 숫자를 입력하지 않은 경우
+     */
+    ERROR_INVALID_EXPECTED_DAY("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+
+    private final String message;
+
+    ExpectedVisitDayErrorMessage(String message) {
+        this.message = ERROR_MESSAGE_PREFIX.getErrorMessagePrefix() + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
