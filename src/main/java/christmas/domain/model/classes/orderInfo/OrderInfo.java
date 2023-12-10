47e7f1aff837d78e392b440ff453976852617dd6
jdohun <wjdehgns97@naver.com>
2023-12-10 19:04:24 +0900

    feat: 입력된 주문 정보를 검증 후 저장하는 기능 구현
    OrderInfo.java

--- src/main/java/christmas/domain/model/classes/orderInfo/OrderInfo.java
+++ src/main/java/christmas/domain/model/classes/orderInfo/OrderInfo.java
@@ -1,12 +1,25 @@
 package christmas.domain.model.classes.orderInfo;
 
-import christmas.domain.model.enums.Menu;
+import christmas.domain.model.enums.menu.Menu;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static christmas.domain.model.classes.orderInfo.OrderInfoErrorMessage.*;
 
 public class OrderInfo {
-    public static final String ORDER_DELIMITER = ",";
-    public static final String ORDER_MENU_DELIMITER = "-";
+    public static final String ORDER_ITEM_DELIMITER  = ",";
+    public static final String ITEM_COUNT_DELIMITER  = "-";
+
+    private static final Pattern ORDER_ITEM_PATTERN = Pattern.compile("^[가-힣]+" + ITEM_COUNT_DELIMITER  + "[1-9][0-9]*$");
+    private static final Pattern INVALID_ORDER_PATTERN = Pattern.compile("^\\s*" + ORDER_ITEM_DELIMITER  + "|" + ORDER_ITEM_DELIMITER  + "\\s*" + ORDER_ITEM_DELIMITER  + "|" + ORDER_ITEM_DELIMITER  + "\\s*$");
+
+    private static final int MAX_TOTAL_COUNT = 20;
 
     private final Map<Menu, Integer> orderInfo;
 
@@ -15,6 +28,98 @@ public class OrderInfo {
     }
 
     public static OrderInfo from(String inputOrderInfo) {
-        return null;
+        Map<Menu, Integer> orderResult = validateAndMapOrderInfo(inputOrderInfo);
+        return new OrderInfo(orderResult);
+    }
+
+    private static Map<Menu, Integer> validateAndMapOrderInfo(String inputOrderInfo) {
+        String[] splitOrderItems = validateAndSplitOrderInfo(inputOrderInfo);
+        List<String> validatedOrderItems = validateAndFormatSplitOrderItems(splitOrderItems);
+        return mapMenuAndCount(validatedOrderItems);
+    }
+
+    private static String[] validateAndSplitOrderInfo(String inputOrderInfo) {
+        validateOrderInfoFormat(inputOrderInfo);
+        return inputOrderInfo.split(ORDER_ITEM_DELIMITER);
+    }
+
+    private static void validateOrderInfoFormat(String inputOrderInfo) {
+        Matcher matcher = INVALID_ORDER_PATTERN.matcher(inputOrderInfo);
+        if (matcher.find()) {
+            throw new IllegalArgumentException(ERROR_INVALID_ORDER.getMessage());
+        }
+    }
+
+    private static List<String> validateAndFormatSplitOrderItems(String[] splitOrders) {
+        return Arrays.stream(splitOrders)
+                .map(menuWithCount -> validateAndFormatOrderItem(menuWithCount.trim()))
+                .collect(Collectors.toList());
+    }
+
+    private static String validateAndFormatOrderItem(String orderItem) {
+        validateOrderItemFormat(orderItem);
+        return orderItem;
+    }
+
+    private static void validateOrderItemFormat(String orderItem) {
+        Matcher matcher = ORDER_ITEM_PATTERN.matcher(orderItem);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ERROR_INVALID_ORDER.getMessage());
+        }
+    }
+
+    private static Map<Menu, Integer> mapMenuAndCount(List<String> validatedOrderItems) {
+        Map<Menu, Integer> orderResult = new HashMap<>();
+        int totalItemCount = 0;
+
+        for (String orderItem : validatedOrderItems) {
+            String[] menuAndCount = orderItem.split(ITEM_COUNT_DELIMITER);
+            String menuName = menuAndCount[0];
+
+            Menu menu = validateOrderItem(menuName, orderResult);
+            int itemCount = Integer.parseInt(menuAndCount[1]);
+
+            validateTotalItemCount(totalItemCount, itemCount);
+
+            orderResult.put(menu, itemCount);
+            totalItemCount += itemCount;
+        }
+
+        validateNotOnlyBeverageOrdered(orderResult);
+        return orderResult;
+    }
+
+    private static Menu validateOrderItem(String menuName, Map<Menu, Integer> result) {
+        validateExistsMenu(menuName);
+        Menu menu = Menu.findByName(menuName);
+        validateDuplicateMenu(menu, result);
+        return menu;
+    }
+
+    private static void validateExistsMenu(String menuName) {
+        if (!Menu.isExistentMenu(menuName)) {
+            throw new IllegalArgumentException(ERROR_INVALID_ORDER.getMessage());
+        }
+    }
+
+    private static void validateDuplicateMenu(Menu menu, Map<Menu, Integer> result) {
+        if (result.containsKey(menu)) {
+            throw new IllegalArgumentException(ERROR_INVALID_ORDER.getMessage());
+        }
+    }
+
+    private static void validateTotalItemCount(int totalItemCount, int itemCount) {
+        if (totalItemCount + itemCount > MAX_TOTAL_COUNT) {
+            throw new IllegalArgumentException(ERROR_OVER_COUNT_OF_MENU.getMessage());
+        }
+    }
+
+    public static void validateNotOnlyBeverageOrdered(Map<Menu, Integer> orderResult) {
+        boolean containsNonBeverage = orderResult.keySet().stream()
+                .anyMatch(menu -> !menu.isBeverage());
+
+        if (!containsNonBeverage) {
+            throw new IllegalArgumentException(ERROR_ORDER_ONLY_BEVERAGE.getMessage());
+        }
     }
 }
