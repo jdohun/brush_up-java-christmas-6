69974ccd48b55b2880ddb189530ca2bfc93f1006
jdohun <wjdehgns97@naver.com>
2023-12-12 21:14:37 +0900

    feat: 함수형 인터페이스를 적용하여 날짜 범위를 확인한 결과, 날짜를 통해 계산된 할인 금액 반환, 총금액이 기준을 충족하는지 확인하는 기능 구현
    DecemberEventPlan.java

--- src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
+++ src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
@@ -1,7 +1,10 @@
 package christmas.domain.model.classes.decemberEventPlan;
 
+import christmas.domain.model.classes.expectedVisitDay.DateBasedDiscountCalculator;
+import christmas.domain.model.classes.expectedVisitDay.EventDateChecker;
 import christmas.domain.model.classes.expectedVisitDay.ExpectedVisitDay;
 import christmas.domain.model.classes.orderInfo.OrderInfo;
+import christmas.domain.model.classes.orderInfo.DiscountCriteriaChecker;
 
 public class DecemberEventPlan {
 
@@ -11,5 +14,21 @@ public class DecemberEventPlan {
     public DecemberEventPlan(ExpectedVisitDay expectedVisitDay, OrderInfo orderInfo) {
         this.expectedVisitDay = expectedVisitDay;
         this.orderInfo = orderInfo;
+    }
+
+    public int calculateTotalAmountBeforeDiscount() {
+        return orderInfo.calculateTotalAmount();
+    }
+
+    public boolean isDateWithinEventPeriod(EventDateChecker eventDateChecker) {
+        return expectedVisitDay.isDateWithinEventPeriod(eventDateChecker);
+    }
+
+    public boolean isTotalAmountSatisfyingCondition(DiscountCriteriaChecker discountCriteriaChecker) {
+        return discountCriteriaChecker.isSatisfied(calculateTotalAmountBeforeDiscount());
+    }
+
+    public int calculateEventBenefitByDate(DateBasedDiscountCalculator dateBasedDiscountCalculator) {
+        return expectedVisitDay.calculateEventBenefitByDate(dateBasedDiscountCalculator);
     }
 }
