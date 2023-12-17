90ea419a030111450b4bb851c4a773aed743e351
jdohun <wjdehgns97@naver.com>
2023-12-17 19:41:25 +0900

    feat: 12월 프로모션 전제 조건을 확인하는 인터페이스 구현 및 상수 위치 이동
    DecemberPromotionPrecondition.java
    PeriodCondition.java
    SpecificDateCondition.java

---
+++ src/main/java/christmas/domain/promotion/precondition/DecemberPromotionPrecondition.java
@@ -0,0 +1,18 @@
+package christmas.domain.promotion.precondition;
+
+import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
+import christmas.domain.promotion.strategy.dateCheckStrategy.PeriodCondition;
+import christmas.domain.promotion.strategy.totalAmountCheckStrategy.TotalAmountCondition;
+
+public interface DecemberPromotionPrecondition {
+    int EVENT_PERIOD_YEAR = 2023;
+    int EVENT_PERIOD_MONTH = 12;
+
+    PeriodCondition PERIOD_CONDITION = PeriodCondition.MONTHLY_DECEMBER;
+    TotalAmountCondition TOTAL_AMOUNT_CONDITION = TotalAmountCondition.MINIMUM_ORDER_AMOUNT;
+
+    static boolean isSatisfyingPrecondition(DecemberEventPlan decemberEventPlan) {
+        return PERIOD_CONDITION.isPlanWithinPeriod(decemberEventPlan)
+                && TOTAL_AMOUNT_CONDITION.isTotalAmountSufficient(decemberEventPlan.getTotalAmountBeforeDiscount());
+    }
+}
