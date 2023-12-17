76b6676c00af5426c60f95fe6c47357de4bb0a78
DH Jeong <wjdehgns97@gmail.com>
2023-12-17 19:30:58 +0900

    feat: LocalDate 기반 할인 계산 전략 인터페이스 구현
    DateBasedDiscountStrategy.java

--- src/main/java/christmas/domain/model/classes/expectedVisitDay/DateBasedDiscountCalculator.java
+++
@@ -1,8 +0,0 @@
-package christmas.domain.model.classes.expectedVisitDay;
-
-import java.time.LocalDate;
-
-@FunctionalInterface
-public interface DateBasedDiscountCalculator {
-    int calculateDiscount(LocalDate date);
-}
