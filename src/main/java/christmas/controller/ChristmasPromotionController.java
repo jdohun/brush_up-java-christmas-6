2d4479184aabd2c0c7ed76383b0a3de62b68cad8
DH Jeong <wjdehgns97@gmail.com>
2023-12-17 16:47:16 +0900

    feat: 가공하여 반환된 DecemberEventPlan 에 프로모션을 적용하는 Controller 구현
    ChristmasPromotionController.java

--- src/main/java/christmas/controller/ChristmasPromotionController.java
+++ src/main/java/christmas/controller/ChristmasPromotionController.java
@@ -2,47 +2,16 @@ package christmas.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
-import christmas.domain.model.classes.expectedVisitDay.ExpectedVisitDay;
-import christmas.domain.model.classes.orderInfo.OrderInfo;
-import christmas.view.inputView.InputView;
 import christmas.view.outputView.OutputView;
 
-import java.util.function.Supplier;
-
 public class ChristmasPromotionController {
-    private static final InputView INPUT_VIEW = InputView.getInstance();
     private static final OutputView OUTPUT_VIEW = OutputView.getInstance();
 
-    public void run() {
+    public void run(DecemberEventPlan decemberEventPlan) {
         try {
-            OUTPUT_VIEW.showGreetings();
-            DecemberEventPlan decemberEventPlan = inputDecemberEventPlan();
+
         } finally {
             releaseResources();
-        }
-    }
-
-    private ExpectedVisitDay inputExpectedVisitDay() {
-        return ExpectedVisitDay.from(INPUT_VIEW.inputExpectedVisitDay());
-    }
-
-    private OrderInfo inputOrderInfo() {
-        return OrderInfo.from(INPUT_VIEW.inputOrderInfo());
-    }
-
-    private DecemberEventPlan inputDecemberEventPlan() {
-        ExpectedVisitDay expectedVisitDay = repeatUntilNoException(this::inputExpectedVisitDay);
-        OrderInfo orderInfo = repeatUntilNoException(this::inputOrderInfo);
-        return new DecemberEventPlan(expectedVisitDay, orderInfo);
-    }
-
-    private <T> T repeatUntilNoException(Supplier<T> action) {
-        while (true) {
-            try {
-                return action.get();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
         }
     }
 
