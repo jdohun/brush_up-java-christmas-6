b65b75ad50d4433299aa6fb3bffaa20d560194c5
jdohun <wjdehgns97@naver.com>
2023-12-17 19:54:07 +0900

    feat: ChristmasDDayPromotion 클래스 구현
    12월 프로모션 조건을 충족하고, 크리스마스 이벤트 기간 내에 적용되는 할인 전략을 제공합니다.
    
    ChristmasDDayPromotion.java

---
+++ src/main/java/christmas/domain/promotion/context/discount/ChristmasDDayPromotion.java
@@ -0,0 +1,30 @@
+package christmas.domain.promotion.context.discount;
+
+import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
+import christmas.dto.DiscountAmount;
+import christmas.domain.promotion.strategy.dateCheckStrategy.PeriodCondition;
+import christmas.domain.promotion.precondition.DecemberPromotionPrecondition;
+import christmas.domain.promotion.strategy.discountStrategy.byDate.DiscountByDate;
+
+public class ChristmasDDayPromotion implements DecemberPromotionPrecondition {
+    public static final int DEFAULT_DISCOUNT_AMOUNT = 1_000;
+    public static final int INCREASING_DISCOUNT_AMOUNT_PER_DAY = 100;
+    private static final PeriodCondition PERIOD_CONDITION = PeriodCondition.UNTIL_CHRISTMAS;
+    private static final DiscountByDate DISCOUNT_STRATEGY_BY_DATE = DiscountByDate.CHRISTMAS_D_DAY;
+
+    public DiscountAmount apply(DecemberEventPlan decemberEventPlan) {
+        if (isApplicable(decemberEventPlan)) {
+            return calculateDiscountAmount(decemberEventPlan);
+        }
+        return new DiscountAmount(0);
+    }
+
+    private boolean isApplicable(DecemberEventPlan decemberEventPlan) {
+        return DecemberPromotionPrecondition.isSatisfyingPrecondition(decemberEventPlan)
+                && PERIOD_CONDITION.isPlanWithinPeriod(decemberEventPlan);
+    }
+
+    private DiscountAmount calculateDiscountAmount(DecemberEventPlan decemberEventPlan) {
+        return decemberEventPlan.calculateDiscountAmountByDate(DISCOUNT_STRATEGY_BY_DATE.getStrategy());
+    }
+}
