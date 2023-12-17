4b78b48b4f741b7655d3d4f8774939203c4bd7bc
jdohun <wjdehgns97@naver.com>
2023-12-17 16:45:02 +0900

    refactor: DecemberEventPlan 생성시 주문 정보로부터 계산된 값을 불변 객체로 저장하고, 반환하는 기능 추가
    DecemberEventPlan.java

--- src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
+++ src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
@@ -3,29 +3,26 @@ package christmas.domain.model.classes.decemberEventPlan;
 import christmas.domain.model.classes.expectedVisitDay.DateBasedDiscountCalculator;
 import christmas.domain.model.classes.expectedVisitDay.EventDateChecker;
 import christmas.domain.model.classes.expectedVisitDay.ExpectedVisitDay;
-import christmas.domain.model.classes.orderInfo.DiscountCriteriaChecker;
 import christmas.domain.model.classes.orderInfo.OrderInfo;
+import christmas.dto.TotalAmount;
 
 public class DecemberEventPlan {
-
     private final ExpectedVisitDay expectedVisitDay;
     private final OrderInfo orderInfo;
+    private final TotalAmount totalAmountBeforeDiscount;
 
     public DecemberEventPlan(ExpectedVisitDay expectedVisitDay, OrderInfo orderInfo) {
         this.expectedVisitDay = expectedVisitDay;
         this.orderInfo = orderInfo;
+        this.totalAmountBeforeDiscount = new TotalAmount(orderInfo.calculateTotalAmount());
     }
 
-    public int calculateTotalAmountBeforeDiscount() {
-        return orderInfo.calculateTotalAmount();
+    public TotalAmount getTotalAmountBeforeDiscount() {
+        return totalAmountBeforeDiscount;
     }
 
-    public boolean isDateWithinEventPeriod(EventDateChecker eventDateChecker) {
+    public boolean isPlanWithinEventPeriod(EventDateChecker eventDateChecker) {
         return expectedVisitDay.isDateWithinEventPeriod(eventDateChecker);
-    }
-
-    public boolean isTotalAmountSatisfyingCondition(DiscountCriteriaChecker discountCriteriaChecker) {
-        return discountCriteriaChecker.isSatisfied(calculateTotalAmountBeforeDiscount());
     }
 
     public int calculateEventBenefitByDate(DateBasedDiscountCalculator dateBasedDiscountCalculator) {
