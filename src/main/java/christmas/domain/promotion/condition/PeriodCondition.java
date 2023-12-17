233c9e86d7b895cfc727e476b6b5710b0164295b
DH Jeong <wjdehgns97@gmail.com>
2023-12-17 16:53:02 +0900

    feat: 기간 조건을 캡슐화하여 구현
    PeriodCondition.java

---
+++ src/main/java/christmas/domain/promotion/condition/PeriodCondition.java
@@ -0,0 +1,38 @@
+package christmas.domain.promotion.condition;
+
+import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
+
+import java.time.LocalDate;
+
+import static christmas.domain.promotion.common.DecemberPromotionDate.EVENT_PERIOD_MONTH;
+import static christmas.domain.promotion.common.DecemberPromotionDate.EVENT_PERIOD_YEAR;
+
+public enum PeriodCondition {
+     UNTIL_CHRISTMAS(
+            LocalDate.of(EVENT_PERIOD_YEAR, EVENT_PERIOD_MONTH, 1),
+            LocalDate.of(EVENT_PERIOD_YEAR, EVENT_PERIOD_MONTH, 25)
+    ),
+    MONTHLY_DECEMBER(
+            LocalDate.of(EVENT_PERIOD_YEAR, EVENT_PERIOD_MONTH, 1),
+            LocalDate.of(EVENT_PERIOD_YEAR, EVENT_PERIOD_MONTH, 31)
+    );
+
+    private final LocalDate periodStarts;
+    private final LocalDate periodEnds;
+
+    PeriodCondition(
+            LocalDate periodStarts,
+            LocalDate periodEnds
+    ) {
+        this.periodStarts = periodStarts;
+        this.periodEnds = periodEnds;
+    }
+
+    public boolean isPlanWithinPeriod(DecemberEventPlan decemberEventPlan) {
+        return decemberEventPlan.isPlanWithinEventPeriod(this::isDateWithinPeriod);
+    }
+
+    private boolean isDateWithinPeriod(LocalDate dateOfPlan) {
+        return !dateOfPlan.isBefore(periodStarts) && !dateOfPlan.isAfter(periodEnds);
+    }
+}
