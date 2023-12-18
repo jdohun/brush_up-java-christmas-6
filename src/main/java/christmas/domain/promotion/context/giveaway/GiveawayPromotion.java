9ac550fdf4efb3db1b7729e357e1cafc2f73013f
DH Jeong <wjdehgns97@gmail.com>
2023-12-18 20:10:36 +0900

    feat: GiveawayPromotion 클래스 구현
    12월 프로모션 조건을 충족하고, 할인 전 총금액 조건을 만족하면 증정품을 제공합니다.
    
    GiveawayPromotion.java

---
+++ src/main/java/christmas/domain/promotion/context/giveaway/GiveawayPromotion.java
@@ -0,0 +1,29 @@
+package christmas.domain.promotion.context.giveaway;
+
+import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
+import christmas.domain.model.enums.Giveaway;
+import christmas.domain.promotion.precondition.DecemberPromotionPrecondition;
+import christmas.domain.promotion.strategy.totalAmountCheckStrategy.TotalAmountCondition;
+import christmas.dto.GiveawayAndQuantityDto;
+
+import java.util.Optional;
+
+public class GiveawayPromotion implements DecemberPromotionPrecondition {
+    private static final TotalAmountCondition TOTAL_AMOUNT_CONDITION = TotalAmountCondition.GIVEAWAY_EVENT_THRESHOLD;
+
+    public Optional<GiveawayAndQuantityDto> apply(DecemberEventPlan decemberEventPlan) {
+        if (isApplicable(decemberEventPlan)) {
+            return Optional.of(createGiveaway());
+        }
+        return Optional.empty();
+    }
+
+    private boolean isApplicable(DecemberEventPlan decemberEventPlan) {
+        return DecemberPromotionPrecondition.isSatisfyingPrecondition(decemberEventPlan)
+                && TOTAL_AMOUNT_CONDITION.isTotalAmountSufficient(decemberEventPlan.getTotalAmountBeforeDiscount());
+    }
+
+    private GiveawayAndQuantityDto createGiveaway() {
+        return new GiveawayAndQuantityDto(Giveaway.GIVEAWAY_CHAMPAGNE, 1);
+    }
+}
