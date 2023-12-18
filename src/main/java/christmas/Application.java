0f8f3cb8a5d6eee4fc22db19a670b6d0d7a21f98
DH Jeong <wjdehgns97@gmail.com>
2023-12-18 21:53:19 +0900

    feat: Main 이 ChristmasPromotionController 을 사용

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,14 @@
 package christmas;
 
 import christmas.controller.ChristmasPromotionController;
+import christmas.controller.DecemberEventPlanController;
+import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
 
 public class Application {
     public static void main(String[] args) {
-        ChristmasPromotionController christmasPromotionController = new ChristmasPromotionController();
-        christmasPromotionController.run();
+        final DecemberEventPlanController decemberEventPlanController = new DecemberEventPlanController();
+        final ChristmasPromotionController christmasPromotionController = new ChristmasPromotionController();
+        final DecemberEventPlan decemberEventPlan = decemberEventPlanController.run();
+        christmasPromotionController.run(decemberEventPlan);
     }
 }
