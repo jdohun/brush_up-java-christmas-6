3d703ad2163d29eecf5ae8746aff8d6158d58678
jdohun <wjdehgns97@naver.com>
2023-12-18 20:14:00 +0900

    style: MenuAndQuantityDto 레코드의 코드 포맷 변경
    가독성 향상을 위해 매개변수를 개행하여 작성합니다.

--- src/main/java/christmas/dto/MenuAndQuantityDto.java
+++ src/main/java/christmas/dto/MenuAndQuantityDto.java
@@ -2,5 +2,8 @@ package christmas.dto;
 
 import christmas.domain.model.enums.menu.Menu;
 
-public record MenuAndQuantityDto(Menu menu, int quantity) {
+public record MenuAndQuantityDto(
+        Menu menu,
+        int quantity
+) {
 }
