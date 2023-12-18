6b181830e034af5d7030a381ff4b1edee55b722d
DH Jeong <wjdehgns97@gmail.com>
2023-12-18 21:48:30 +0900

    refactor: ChristmasDDayPromotion 리팩터링
    상속된 인터페이스명 변경에 따라 동일하게 변경
    프로모션 이름을 가지도록 함
    변경된 할인 전략 기능을 사용하도록 함

--- src/main/java/christmas/domain/promotion/context/discount/ChristmasDDayPromotion.java
+++ src/main/java/christmas/domain/promotion/context/discount/ChristmasDDayPromotion.java
@@ -1,18 +1,20 @@
 package christmas.domain.promotion.context.discount;
 
 import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
-import christmas.domain.promotion.precondition.DecemberPromotionPrecondition;
+import christmas.domain.promotion.precondition.ChristmasPromotionPrecondition;
 import christmas.domain.promotion.strategy.dateCheckStrategy.PeriodCondition;
 import christmas.domain.promotion.strategy.discountStrategy.byDate.DiscountByDate;
 import christmas.dto.DiscountAmount;
 
 import java.util.Optional;
 
-public class ChristmasDDayPromotion implements DecemberPromotionPrecondition {
+public class ChristmasDDayPromotion implements ChristmasPromotionPrecondition {
     public static final int DEFAULT_DISCOUNT_AMOUNT = 1_000;
     public static final int INCREASING_DISCOUNT_AMOUNT_PER_DAY = 100;
     private static final PeriodCondition PERIOD_CONDITION = PeriodCondition.UNTIL_CHRISTMAS;
     private static final DiscountByDate DISCOUNT_STRATEGY_BY_DATE = DiscountByDate.CHRISTMAS_D_DAY;
+
+    private static final String PROMOTION_NAME = "크리스마스 디데이 할인";
 
     private ChristmasDDayPromotion() {
     }
@@ -29,12 +31,12 @@ public class ChristmasDDayPromotion implements DecemberPromotionPrecondition {
     }
 
     private boolean isApplicable(DecemberEventPlan decemberEventPlan) {
-        return DecemberPromotionPrecondition.isSatisfyingPrecondition(decemberEventPlan)
+        return ChristmasPromotionPrecondition.isSatisfyingPrecondition(decemberEventPlan)
                 && PERIOD_CONDITION.isPlanWithinPeriod(decemberEventPlan);
     }
 
     private DiscountAmount calculateDiscountAmount(DecemberEventPlan decemberEventPlan) {
-        return decemberEventPlan.calculateDiscountAmountByDate(DISCOUNT_STRATEGY_BY_DATE.getStrategy());
+        return DISCOUNT_STRATEGY_BY_DATE.applyDiscount(decemberEventPlan);
     }
 
     private static class Holder {
