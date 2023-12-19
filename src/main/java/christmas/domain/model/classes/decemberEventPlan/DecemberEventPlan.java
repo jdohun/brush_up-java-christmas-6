9ef3123105dcb0507aa6b193f1086d335e268558
DH Jeong <wjdehgns97@gmail.com>
2023-12-19 21:49:17 +0900

    refactor: DecemberEventPlan
    계산된 할인 전 총금액을 저장하지 않고 계산하여 그대로 반환합니다.
    
    이를 통해 메뉴 가격의 변동에 따라 정보가 유동적으로 반영됩니다.

--- src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
+++ src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
@@ -1,33 +1,30 @@
 package christmas.domain.model.classes.decemberEventPlan;
 
-import christmas.domain.promotion.strategy.discountStrategy.byDate.DateBasedDiscountStrategy;
-import christmas.domain.promotion.strategy.dateCheckStrategy.DateCheckStrategy;
 import christmas.domain.model.classes.expectedVisitDay.ExpectedVisitDay;
 import christmas.domain.model.classes.orderInfo.OrderInfo;
-import christmas.dto.DiscountAmount;
+import christmas.domain.promotion.strategy.dateCheckStrategy.DateCheckStrategy;
+import christmas.domain.promotion.strategy.discountStrategy.byDate.DateBasedDiscountStrategy;
 import christmas.dto.TotalAmount;
 
 public class DecemberEventPlan {
     private final ExpectedVisitDay expectedVisitDay;
     private final OrderInfo orderInfo;
-    private final TotalAmount totalAmountBeforeDiscount;
 
     public DecemberEventPlan(ExpectedVisitDay expectedVisitDay, OrderInfo orderInfo) {
         this.expectedVisitDay = expectedVisitDay;
         this.orderInfo = orderInfo;
-        this.totalAmountBeforeDiscount = new TotalAmount(orderInfo.calculateTotalAmount());
     }
 
     public TotalAmount getTotalAmountBeforeDiscount() {
-        return totalAmountBeforeDiscount;
+        return new TotalAmount(orderInfo.calculateTotalAmount());
     }
 
     public boolean isDateSatisfyingDateCondition(DateCheckStrategy dateCheckStrategy) {
         return expectedVisitDay.isSatisfyingCondition(dateCheckStrategy);
     }
 
-    public DiscountAmount calculateDiscountAmountByDate(DateBasedDiscountStrategy dateBasedDiscountStrategy) {
-        return new DiscountAmount(expectedVisitDay.calculateDiscountAmountByDate(dateBasedDiscountStrategy));
+    public int calculateDiscountAmountByDate(DateBasedDiscountStrategy dateBasedDiscountStrategy) {
+        return expectedVisitDay.calculateDiscountAmountByDate(dateBasedDiscountStrategy);
     }
 
     public int countMainMenus() {
