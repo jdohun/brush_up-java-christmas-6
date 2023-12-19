79c0b969af3fef318b270fcc8268d4e89809c71d
DH Jeong <wjdehgns97@gmail.com>
2023-12-19 21:50:34 +0900

    refactor: ChristmasPromotionPrecondition
    상수명을 간단하게 변경합니다.
    PeriodCondition 의 DecemberEventPlanConditionChecker 상속에 따라 메소드명을 변경합니다.
    
    이를 통해 코드를 이해하기 쉬워질 것으로 예상합니다.

--- src/main/java/christmas/domain/promotion/precondition/ChristmasPromotionPrecondition.java
+++ src/main/java/christmas/domain/promotion/precondition/ChristmasPromotionPrecondition.java
@@ -1,18 +1,18 @@
 package christmas.domain.promotion.precondition;
 
 import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
-import christmas.domain.promotion.strategy.dateCheckStrategy.PeriodCondition;
+import christmas.domain.promotion.strategy.dateCheckStrategy.impl.PeriodCondition;
 import christmas.domain.promotion.strategy.totalAmountCheckStrategy.TotalAmountCondition;
 
 public interface ChristmasPromotionPrecondition {
-    int EVENT_PERIOD_YEAR = 2023;
-    int EVENT_PERIOD_MONTH = 12;
+    int EVENT_YEAR = 2023;
+    int EVENT_MONTH = 12;
 
     PeriodCondition PERIOD_CONDITION = PeriodCondition.MONTHLY_DECEMBER;
     TotalAmountCondition TOTAL_AMOUNT_CONDITION = TotalAmountCondition.MINIMUM_ORDER_AMOUNT;
 
     static boolean isSatisfyingPrecondition(DecemberEventPlan decemberEventPlan) {
-        return PERIOD_CONDITION.isPlanWithinPeriod(decemberEventPlan)
+        return PERIOD_CONDITION.isPlanSatisfyingCondition(decemberEventPlan)
                 && TOTAL_AMOUNT_CONDITION.isTotalAmountSufficient(decemberEventPlan.getTotalAmountBeforeDiscount());
     }
 }
