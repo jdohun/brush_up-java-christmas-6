969da63f46a4c5e0b02c91785d4d867c6798b0cf
DH Jeong <wjdehgns97@gmail.com>
2023-12-19 21:36:42 +0900

    refactor: PeriodCondition, DayOfWeekCondition 리팩터링
    DecemberEventPlanConditionChecker 를 상속합니다.
    상속을 구현한 파일을 저장하는 impl 패키지로 이동합니다.

--- src/main/java/christmas/domain/promotion/strategy/dateCheckStrategy/impl/DayOfWeekCondition.java
+++ src/main/java/christmas/domain/promotion/strategy/dateCheckStrategy/impl/DayOfWeekCondition.java
@@ -1,10 +1,11 @@
-package christmas.domain.promotion.strategy.dateCheckStrategy;
+package christmas.domain.promotion.strategy.dateCheckStrategy.impl;
 
 import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
+import christmas.domain.promotion.strategy.dateCheckStrategy.DateCheckStrategy;
 
 import java.time.DayOfWeek;
 
-public enum DayOfWeekCondition {
+public enum DayOfWeekCondition implements DecemberEventPlanConditionChecker {
     IS_WEEKDAY(dateOfPlan -> dateOfPlan.getDayOfWeek() != DayOfWeek.FRIDAY && dateOfPlan.getDayOfWeek() != DayOfWeek.SATURDAY),
     IS_WEEKEND(dateOfPlan -> dateOfPlan.getDayOfWeek() == DayOfWeek.FRIDAY || dateOfPlan.getDayOfWeek() == DayOfWeek.SATURDAY);
 
@@ -14,6 +15,7 @@ public enum DayOfWeekCondition {
         this.dateCheckStrategy = dateCheckStrategy;
     }
 
+    @Override
     public boolean isPlanSatisfyingCondition(DecemberEventPlan decemberEventPlan) {
         return decemberEventPlan.isDateSatisfyingDateCondition(dateCheckStrategy);
     }
