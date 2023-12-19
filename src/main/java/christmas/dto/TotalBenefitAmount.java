7a336519e959709aab93ae51a1f029721ce6e440
jdohun <wjdehgns97@naver.com>
2023-12-19 21:41:45 +0900

    feat: TotalBenefitAmount 레코드 추가
    총할인 금액을 불변 객체로 저장합니다.

---
+++ src/main/java/christmas/dto/TotalBenefitAmount.java
@@ -0,0 +1,10 @@
+package christmas.dto;
+
+public record TotalBenefitAmount(int totalBenefitAmount) {
+//    public static TotalBenefitAmount of(TotalDiscountAmount totalDiscountAmount, ) {
+//        int totalBenefitAmount = Arrays.stream(discountInfos)
+//                .mapToInt(DiscountInfo::discountAmount)
+//                .sum();
+//        return new TotalBenefitAmount(totalBenefitAmount);
+//    }
+}
