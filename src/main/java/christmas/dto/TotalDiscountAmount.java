8da4bf0d9a8cbce06f3faeca0f7476def48a5d4e
jdohun <wjdehgns97@naver.com>
2023-12-19 21:42:27 +0900

    refactor: TotalDiscountAmount 리팩터링
    DiscountAmount 의 이름 변경에 따라 수정합니다.

--- src/main/java/christmas/dto/TotalDiscountAmount.java
+++ src/main/java/christmas/dto/TotalDiscountAmount.java
@@ -3,9 +3,9 @@ package christmas.dto;
 import java.util.Arrays;
 
 public record TotalDiscountAmount(int totalDiscountAmount) {
-    public static TotalDiscountAmount of(DiscountAmount... discountAmounts) {
-        int totalDiscountAmount = Arrays.stream(discountAmounts)
-                .mapToInt(DiscountAmount::discountAmount)
+    public static TotalDiscountAmount of(DiscountInfo... discountInfos) {
+        int totalDiscountAmount = Arrays.stream(discountInfos)
+                .mapToInt(DiscountInfo::discountAmount)
                 .sum();
         return new TotalDiscountAmount(totalDiscountAmount);
     }
