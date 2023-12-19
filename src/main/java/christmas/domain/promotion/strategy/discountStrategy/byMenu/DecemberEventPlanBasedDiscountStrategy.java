03b860d7e473baba7a85536794588de788096161
DH Jeong <wjdehgns97@gmail.com>
2023-12-19 21:21:17 +0900

    refactor: 사용하지 않는 파일 제거
    DecemberEventPlanBasedDiscountStrategy.java
    DiscountCriteriaChecker.java

--- src/main/java/christmas/domain/promotion/strategy/discountStrategy/byMenu/DecemberEventPlanBasedDiscountStrategy.java
+++
@@ -1,8 +0,0 @@
-package christmas.domain.promotion.strategy.discountStrategy.byMenu;
-
-import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
-
-@FunctionalInterface
-public interface DecemberEventPlanBasedDiscountStrategy {
-    int calculateDiscountAmount(DecemberEventPlan decemberEventPlan);
-}
