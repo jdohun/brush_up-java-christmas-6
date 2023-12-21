9266bd8e2e684259de996be2fa625cf1a7994ee5
jdohun <wjdehgns97@naver.com>
2023-12-21 21:25:56 +0900

    feat: DiscountPromotionType 에 applyPromotionTypes 메소드 추가
    1.`DecemberEventPlan` 을 전달 받습니다.
    2. 모든 할인 프로모션 타입이 각각 주어진 `DecemberEventPlan` 에 프로모션을 적용합니다.
    3. 반환된 `Optional` 이 `ifPresent` 하면 `List<DiscountInfo>`에 추가합니다.
    4. 저장이 완료된 `List<DiscountInfo>` 를 반환합니다.

--- src/main/java/christmas/domain/promotion/enums/DiscountPromotionType.java
+++ src/main/java/christmas/domain/promotion/enums/DiscountPromotionType.java
@@ -1,10 +1,16 @@
 package christmas.domain.promotion.enums;
 
+import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
 import christmas.domain.promotion.context.discount.DiscountPromotion;
 import christmas.domain.promotion.context.discount.impl.ChristmasDDayPromotion;
 import christmas.domain.promotion.context.discount.impl.SpecialDayPromotion;
 import christmas.domain.promotion.context.discount.impl.WeekdayPromotion;
 import christmas.domain.promotion.context.discount.impl.WeekendPromotion;
+import christmas.dto.DiscountInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public enum DiscountPromotionType {
     CHRISTMAS_D_DAY(ChristmasDDayPromotion.getInstance()),
@@ -17,5 +23,17 @@ public enum DiscountPromotionType {
 
     DiscountPromotionType(DiscountPromotion promotion) {
         this.promotion = promotion;
+    }
+
+    public List<DiscountInfo> applyPromotionTypes(DecemberEventPlan decemberEventPlan) {
+        List<DiscountInfo> discountInfoList = new ArrayList<>();
+
+        for (DiscountPromotionType discountPromotionType : DISCOUNT_PROMOTION_TYPES) {
+            Optional<DiscountInfo> optionalDiscountInfo = discountPromotionType.promotion.apply(decemberEventPlan);
+
+            optionalDiscountInfo.ifPresent(discountInfoList::add);
+        }
+
+        return discountInfoList;
     }
 }
