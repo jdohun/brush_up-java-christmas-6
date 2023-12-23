076d6647271aab121f7d79a6155de20907836e88
DH Jeong <wjdehgns97@gmail.com>
2023-12-23 19:11:07 +0900

    refactor: TotalBenefitAmount 리팩터링
    - 변수명 수정: 저장하는 총할인 금액 변수명을 amount 로 간결하게 수정합니다.
    - 메소드 수정: 전달받은 `TotalDiscountAmount` 와 `TotalGiveawayAmount`의 각 amount 를 더한 값을 통해 TotalBenefitAmount 생성합니다.

--- src/main/java/christmas/dto/TotalBenefitAmount.java
+++ src/main/java/christmas/dto/TotalBenefitAmount.java
@@ -1,10 +1,8 @@
 package christmas.dto;
 
-public record TotalBenefitAmount(int totalBenefitAmount) {
-//    public static TotalBenefitAmount of(TotalDiscountAmount totalDiscountAmount, ) {
-//        int totalBenefitAmount = Arrays.stream(discountInfos)
-//                .mapToInt(DiscountInfo::discountAmount)
-//                .sum();
-//        return new TotalBenefitAmount(totalBenefitAmount);
-//    }
+public record TotalBenefitAmount(int amount) {
+    public static TotalBenefitAmount of(TotalDiscountAmount totalDiscountAmount, TotalGiveawayAmount totalGiveawayAmount) {
+        int totalBenefitAmount = totalDiscountAmount.amount() + totalGiveawayAmount.amount();
+        return new TotalBenefitAmount(totalBenefitAmount);
+    }
 }
