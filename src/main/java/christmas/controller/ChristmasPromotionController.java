1e7e62fd69e65f0a781cdbd59f9e9a6df4d138f3
jdohun <wjdehgns97@naver.com>
2023-12-23 19:41:34 +0900

    feat: ChristmasPromotionController 기능 추가 및 수정
    - 미리 보기 출력을 위해 `Preview` 를 소유합니다.
    - 전달받은 `DecemberEventPlan`를 모든 크리스마스 프로모션에 적용 후 그 결과를 `Preview`를 통해 출력합니다.

--- src/main/java/christmas/controller/ChristmasPromotionController.java
+++ src/main/java/christmas/controller/ChristmasPromotionController.java
@@ -1,26 +1,51 @@
 package christmas.controller;
 
 import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
+import christmas.domain.model.classes.discountInfos.DiscountInfos;
+import christmas.domain.model.classes.giveawayInofs.GiveawayInfos;
+import christmas.domain.model.enums.Badge;
 import christmas.domain.promotion.enums.DiscountPromotionType;
 import christmas.domain.promotion.enums.GiveawayPromotionType;
-import christmas.dto.DiscountInfo;
-import christmas.dto.GiveawayInfo;
-import christmas.view.outputView.OutputView;
-
-import java.util.List;
+import christmas.dto.ChristmasPromotionPreview;
+import christmas.dto.TotalBenefitAmount;
+import christmas.dto.TotalDiscountAmount;
+import christmas.dto.TotalGiveawayAmount;
+import christmas.view.outputView.preview.Preview;
 
 public class ChristmasPromotionController {
-    private static final OutputView OUTPUT_VIEW = OutputView.getInstance();
+    private static final Preview PREVIEW = Preview.getInstance();
 
     public void run(DecemberEventPlan decemberEventPlan) {
-
+        final DiscountInfos discountInfos = applyDiscountPromotion(decemberEventPlan);
+        final GiveawayInfos giveawayInfos = applyGiveawayPromotion(decemberEventPlan);
+        final ChristmasPromotionPreview christmasPromotionPreview = createChristmasPromotionPreview(decemberEventPlan, giveawayInfos, discountInfos);
+        PREVIEW.printPreview(christmasPromotionPreview);
     }
 
-    private void applyDiscountPromotion(DecemberEventPlan decemberEventPlan) {
-        final List<DiscountInfo> discountInfos = DiscountPromotionType.applyPromotionTypes(decemberEventPlan);
+    private DiscountInfos applyDiscountPromotion(DecemberEventPlan decemberEventPlan) {
+        return DiscountPromotionType.applyPromotionTypes(decemberEventPlan);
     }
 
-    private void applyGiveawayPromotion(DecemberEventPlan decemberEventPlan) {
-        final List<GiveawayInfo> giveawayInfos = GiveawayPromotionType.applyPromotionTypes(decemberEventPlan);
+    private GiveawayInfos applyGiveawayPromotion(DecemberEventPlan decemberEventPlan) {
+        return GiveawayPromotionType.applyPromotionTypes(decemberEventPlan);
+    }
+
+    private ChristmasPromotionPreview createChristmasPromotionPreview(DecemberEventPlan decemberEventPlan, GiveawayInfos giveawayInfos, DiscountInfos discountInfos) {
+        final TotalDiscountAmount totalDiscountAmount = discountInfos.calculateTotalDiscountAmount();
+        final TotalGiveawayAmount totalGiveawayAmount = giveawayInfos.calculateTotalGiveawayAmount();
+        final TotalBenefitAmount totalBenefitAmount = TotalBenefitAmount.of(totalDiscountAmount, totalGiveawayAmount);
+        final Badge badge = Badge.getByTotalBenefitAmount(totalBenefitAmount);
+
+        return ChristmasPromotionPreview.builder()
+                .expectedVisitDay(decemberEventPlan.getExpectedVisitDayDto())
+                .orderInfo(decemberEventPlan.getOrderInfoDtoList())
+                .totalAmount(decemberEventPlan.getTotalAmountBeforeDiscount())
+                .giveawayInfo(giveawayInfos)
+                .discountInfos(discountInfos)
+                .totalGiveawayAmount(totalGiveawayAmount)
+                .totalDiscountAmount(totalDiscountAmount)
+                .totalBenefitAmount(totalBenefitAmount)
+                .badge(badge)
+                .build();
     }
 }
