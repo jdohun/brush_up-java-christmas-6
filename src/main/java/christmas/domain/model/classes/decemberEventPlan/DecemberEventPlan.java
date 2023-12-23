c98ed691f990094e1b3a9b87d0ec13642d43926a
jdohun <wjdehgns97@naver.com>
2023-12-23 19:05:07 +0900

    feat: DecemberEventPlan 에 getExpectedVisitDayDto() 와 getOrderInfoDtoList() 추가
    각각 저장된 예상 방문 날짜와 주문 정보를 `DTO`로 변환하여 반환합니다.

--- src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
+++ src/main/java/christmas/domain/model/classes/decemberEventPlan/DecemberEventPlan.java
@@ -4,7 +4,11 @@ import christmas.domain.model.classes.expectedVisitDay.ExpectedVisitDay;
 import christmas.domain.model.classes.orderInfo.OrderInfo;
 import christmas.domain.promotion.strategy.dateCheckStrategy.DateCheckStrategy;
 import christmas.domain.promotion.strategy.discountStrategy.byDate.DateBasedDiscountStrategy;
+import christmas.dto.ExpectedVisitDayDto;
+import christmas.dto.MenuAndQuantityDto;
 import christmas.dto.TotalAmount;
+
+import java.util.List;
 
 public class DecemberEventPlan {
     private final ExpectedVisitDay expectedVisitDay;
@@ -33,5 +37,13 @@ public class DecemberEventPlan {
 
     public int countDessertMenus() {
         return orderInfo.countDessertMenus();
+    }
+
+    public ExpectedVisitDayDto getExpectedVisitDayDto() {
+        return expectedVisitDay.toDto();
+    }
+
+    public List<MenuAndQuantityDto> getOrderInfoDtoList() {
+        return orderInfo.toDtoList();
     }
 }
