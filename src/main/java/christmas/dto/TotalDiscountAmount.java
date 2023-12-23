213a0841d1bc05c26dc08532b169bb4535bb3144
DH Jeong <wjdehgns97@gmail.com>
2023-12-23 19:10:14 +0900

    refactor: TotalDiscountAmount 리팩터링
    - 변수명 수정: 저장하는 총할인 금액 변수명을 amount 로 간결하게 수정합니다.
    - 메소드 추가: 전달받은 `List<DiscountInfo>`를 통해 TotalDiscountAmount 를 생성합니다.

--- src/main/java/christmas/dto/TotalDiscountAmount.java
+++ src/main/java/christmas/dto/TotalDiscountAmount.java
@@ -1,12 +1,21 @@
 package christmas.dto;
 
 import java.util.Arrays;
+import java.util.List;
 
-public record TotalDiscountAmount(int totalDiscountAmount) {
+public record TotalDiscountAmount(int amount) {
     public static TotalDiscountAmount of(DiscountInfo... discountInfos) {
         int totalDiscountAmount = Arrays.stream(discountInfos)
                 .mapToInt(DiscountInfo::discountAmount)
                 .sum();
         return new TotalDiscountAmount(totalDiscountAmount);
     }
+
+    public static TotalDiscountAmount from(List<DiscountInfo> discountInfos) {
+        int totalDiscountAmount = discountInfos.stream()
+                .mapToInt(discountInfo -> discountInfo.discountAmount())
+                .sum();
+        return new TotalDiscountAmount(totalDiscountAmount);
+    }
+
 }
