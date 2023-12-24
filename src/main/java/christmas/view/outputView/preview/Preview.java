d0af9512cbde84085352ccd64d6269ca0f898f01
DH Jeong <wjdehgns97@gmail.com>
2023-12-24 15:01:12 +0900

    feat: `Preview`에서 전달 받은 `ChristmasPromotionPreview`를 출력하는 기능 추가
    - printPreview : 전체 내용을 출력합니다.
    - printPreviewTitle : 미리보기 타이틀을 출력합니다.
    - printOrderInfo : 타이틀과 함께 주문 메뉴를 출력합니다.
    - printTotalAmountBeforeDiscount : 타이틀과 함께 할인 전 총주문 금액을 출력합니다.
    - printGiveawayInfos : 타이틀과 함께 증정 메뉴를 출력합니다. 증정 메뉴가 존재하지 않는다면 "없음"을 출력합니다.
    - printBenefitInfo : 타이틀과 함께 혜택 내역을 출력합니다. 혜택이 존재하지 않으면 "없음"을 출력합니다.
    - printTotalBenefitAmount : 타이틀과 함께 총 헤택금액을 음수로 금액 표기 형식으로 출력합니다.
    - printTotalAmountAfterDiscount : 타이틀과 함께 할인 후 예상 결제 금액을 금액 표기 형식으로 출력합니다.
    -
     printOptionalBadge : 타이틀과 함께 뱃지 이름을 출력합니다. 값이 존재하지 않으면 "없음"을 출력합니다.

--- src/main/java/christmas/view/outputView/preview/Preview.java
+++ src/main/java/christmas/view/outputView/preview/Preview.java
@@ -1,6 +1,12 @@
 package christmas.view.outputView.preview;
 
-import christmas.dto.ChristmasPromotionPreview;
+import christmas.domain.model.classes.discountInfos.DiscountInfos;
+import christmas.domain.model.classes.giveawayInofs.GiveawayInfos;
+import christmas.domain.model.enums.Badge;
+import christmas.dto.*;
+
+import java.util.List;
+import java.util.Optional;
 
 import static christmas.view.outputView.preview.PreviewMessage.*;
 
@@ -13,7 +19,97 @@ public final class Preview {
     }
 
     public void printPreview(ChristmasPromotionPreview christmasPromotionPreview) {
+        printPreviewTitle(christmasPromotionPreview.expectedVisitDay());
+        printOrderInfo(christmasPromotionPreview.orderInfo());
+        printTotalAmountBeforeDiscount(christmasPromotionPreview.totalAmount());
+        printGiveawayInfos(christmasPromotionPreview.giveawayInfos());
+        printBenefitInfo(christmasPromotionPreview.giveawayInfos(), christmasPromotionPreview.discountInfos());
+        printTotalBenefitAmount(christmasPromotionPreview.totalBenefitAmount());
+        printTotalAmountAfterDiscount(christmasPromotionPreview.totalAmount(), christmasPromotionPreview.totalDiscountAmount());
+        printOptionalBadge(christmasPromotionPreview.optionalBadge());
+    }
 
+    private void printPreviewTitle(ExpectedVisitDayDto expectedVisitDayDto) {
+        System.out.println(String.format(EVENT_PREVIEW_TITLE_FORMAT.getMessage(), expectedVisitDayDto.date().getDayOfMonth()));
+    }
+
+    private void printOrderInfo(List<MenuAndQuantityDto> orderInfo) {
+        final String menuFormat = MENU_FORMAT.getMessage();
+
+        System.out.println(ORDER_MENU_TITLE.getMessage());
+        orderInfo.forEach(
+                menuAndQuantityDto -> System.out.println(
+                        String.format(
+                                menuFormat,
+                                menuAndQuantityDto.menu().getName(),
+                                menuAndQuantityDto.quantity()
+                        )));
+    }
+
+    private void printTotalAmountBeforeDiscount(TotalAmount totalAmount) {
+        System.out.println(TOTAL_PRICE_BEFORE_DISCOUNT_TITLE.getMessage());
+        System.out.println(String.format(MONEY_FORMAT.getMessage(), totalAmount.amount()));
+    }
+
+    private void printGiveawayInfos(GiveawayInfos giveawayInfos) {
+        final String menuFormat = MENU_FORMAT.getMessage();
+        final List<GiveawayInfo> giveawayInfoList = giveawayInfos.giveawayInfoList();
+
+        System.out.println(GIVEAWAY_MENU_TITLE.getMessage());
+
+        if (!giveawayInfoList.isEmpty()) {
+            giveawayInfoList.forEach(
+                    giveawayInfo -> System.out.println(
+                            String.format(menuFormat, giveawayInfo.giveaway().getName(), giveawayInfo.quantity())
+                    ));
+        }
+
+        if (giveawayInfoList.isEmpty()) {
+            System.out.println(NONE.getMessage());
+        }
+    }
+
+    private void printBenefitInfo(GiveawayInfos giveawayInfos, DiscountInfos discountInfos) {
+        final String benefitDetailsFormat = BENEFIT_DETAILS_FORMAT.getMessage();
+        final List<GiveawayInfo> giveawayInfoList = giveawayInfos.giveawayInfoList();
+        final List<DiscountInfo> discountInfoList = discountInfos.discountInfoList();
+
+        System.out.println(BENEFIT_DETAILS_TITLE.getMessage());
+
+        if (!discountInfoList.isEmpty()) {
+            discountInfoList.forEach(
+                    discountInfo -> System.out.println(
+                            String.format(benefitDetailsFormat, discountInfo.promotionName(), discountInfo.discountAmount())
+                    ));
+        }
+
+        if (!giveawayInfoList.isEmpty()) {
+            giveawayInfoList.forEach(
+                    giveawayInfo -> System.out.println(
+                            String.format(benefitDetailsFormat, giveawayInfo.promotionName(), giveawayInfo.calculateGiveawayAmount())
+                    ));
+        }
+
+        if (discountInfoList.isEmpty() && giveawayInfoList.isEmpty()) {
+            System.out.println(NONE.getMessage());
+        }
+    }
+
+    private void printTotalBenefitAmount(TotalBenefitAmount totalBenefitAmount) {
+        final String benefitMoneyFormat = BENEFIT_MONEY_FORMAT.getMessage();
+
+        System.out.println(TOTAL_BENEFIT_AMOUNT_TITLE.getMessage());
+        System.out.println(String.format(benefitMoneyFormat, totalBenefitAmount.amount()));
+    }
+
+    private void printTotalAmountAfterDiscount(TotalAmount totalAmount, TotalDiscountAmount totalDiscountAmount) {
+        System.out.println(ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT_TITLE.getMessage());
+        System.out.println(String.format(MONEY_FORMAT.getMessage(), totalAmount.amount() - totalDiscountAmount.amount()));
+    }
+
+    private void printOptionalBadge(Optional<Badge> optionalBadge) {
+        System.out.println(DECEMBER_EVENT_BADGE_TITLE.getMessage());
+        System.out.println(optionalBadge.map(Badge::getName).orElse(NONE.getMessage()));
     }
 
     private class Holder {
