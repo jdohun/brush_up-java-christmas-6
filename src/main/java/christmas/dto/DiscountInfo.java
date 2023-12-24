3e80aac225754b17413cdfe1d1efbf5dcfbc5395
DH Jeong <wjdehgns97@gmail.com>
2023-12-24 14:20:36 +0900

    style: `DiscountInfo`의 코드 스타일 수정
    레코드 매개변수를 개행하여 작성함으로써 가독성을 향상시킵니다.

--- src/main/java/christmas/dto/DiscountInfo.java
+++ src/main/java/christmas/dto/DiscountInfo.java
@@ -2,5 +2,8 @@ package christmas.dto;
 
 import christmas.domain.promotion.enums.PromotionName;
 
-public record DiscountInfo(PromotionName promotionName, int discountAmount) {
+public record DiscountInfo(
+        PromotionName promotionName,
+        int discountAmount
+) {
 }
