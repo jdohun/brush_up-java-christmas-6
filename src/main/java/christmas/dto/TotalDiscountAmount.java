124e81115c702f9d8a31bf3b91bdef55a8476e50
jdohun <wjdehgns97@naver.com>
2023-12-28 17:53:43 +0900

    refactor: `TotalDiscountAmount` 에서 사용하지 않는 메서드 제거

--- src/main/java/christmas/dto/TotalDiscountAmount.java
+++ src/main/java/christmas/dto/TotalDiscountAmount.java
@@ -1,21 +1,12 @@
 package christmas.dto;
 
-import java.util.Arrays;
 import java.util.List;
 
 public record TotalDiscountAmount(int amount) {
-    public static TotalDiscountAmount of(DiscountInfo... discountInfos) {
-        int totalDiscountAmount = Arrays.stream(discountInfos)
-                .mapToInt(DiscountInfo::discountAmount)
-                .sum();
-        return new TotalDiscountAmount(totalDiscountAmount);
-    }
-
     public static TotalDiscountAmount from(List<DiscountInfo> discountInfos) {
         int totalDiscountAmount = discountInfos.stream()
                 .mapToInt(discountInfo -> discountInfo.discountAmount())
                 .sum();
         return new TotalDiscountAmount(totalDiscountAmount);
     }
-
 }
