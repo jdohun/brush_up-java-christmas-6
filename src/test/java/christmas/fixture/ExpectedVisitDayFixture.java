a171dd104feef5625d94ba73cf3b1fb9451cebf1
jdohun <wjdehgns97@naver.com>
2024-01-01 14:51:31 +0900

    feat: ExpectedVisitDayFixture 와 DecemberEventPlanFixtureByDay 에 새로운 열거 상수 추가
    - 1~31일에 대한 열거 상수를 추가합니다.

--- src/test/java/christmas/fixture/ExpectedVisitDayFixture.java
+++ src/test/java/christmas/fixture/ExpectedVisitDayFixture.java
@@ -13,7 +13,8 @@ public enum ExpectedVisitDayFixture {
     SPECIAL_DAYS(new int[]{3, 10, 17, 24, 25, 31}),
     NOT_SPECIAL_DAYS(new int[]{1, 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30}),
     UNTIL_CHRISTMAS(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}),
-    AFTER_CHRISTMAS(new int[]{26, 27, 28, 29, 30, 31});
+    AFTER_CHRISTMAS(new int[]{26, 27, 28, 29, 30, 31}),
+    MONTHLY_DECEMBER(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
 
     int[] daysOfMonth;
 
