9b91f7bc8df0fc44aa7f54958ca831d124f62a0e
DH Jeong <wjdehgns97@gmail.com>
2024-01-01 20:40:16 +0900

    refactor: DayOfWeekCondition, PeriodCondition, SpecificDateCondition 에 `DateCheckStrategy`를 상속 구현
    - `DateCheckStrategy`를 상속 구현하여 DateCheckStrategy 를 다루고 있음을 더욱 명확하게 명시합니다.

--- src/main/java/christmas/domain/promotion/strategy/dateCheckStrategy/impl/SpecificDateCondition.java
+++ src/main/java/christmas/domain/promotion/strategy/dateCheckStrategy/impl/SpecificDateCondition.java
@@ -1,6 +1,8 @@
 package christmas.domain.promotion.strategy.dateCheckStrategy.impl;
 
 import christmas.domain.model.classes.decemberEventPlan.DecemberEventPlan;
+import christmas.domain.promotion.strategy.dateCheckStrategy.DateCheckStrategy;
+import christmas.domain.promotion.strategy.dateCheckStrategy.DecemberEventPlanConditionChecker;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -8,7 +10,7 @@ import java.util.Set;
 import static christmas.domain.promotion.context.ChristmasPromotionPrecondition.EVENT_MONTH;
 import static christmas.domain.promotion.context.ChristmasPromotionPrecondition.EVENT_YEAR;
 
-public enum SpecificDateCondition implements DecemberEventPlanConditionChecker {
+public enum SpecificDateCondition implements DecemberEventPlanConditionChecker, DateCheckStrategy {
     SPECIAL_DATES(Set.of(
             LocalDate.of(EVENT_YEAR, EVENT_MONTH, 3),
             LocalDate.of(EVENT_YEAR, EVENT_MONTH, 10),
@@ -24,13 +26,20 @@ public enum SpecificDateCondition implements DecemberEventPlanConditionChecker {
         this.specificDates = specificDates;
     }
 
+    // DateCheckStrategy
     private boolean hasMatchingDate(LocalDate dateOfPlan) {
         return specificDates.contains(dateOfPlan);
     }
 
     @Override
     public boolean isPlanSatisfyingCondition(DecemberEventPlan decemberEventPlan) {
-        return decemberEventPlan.isDateSatisfyingDateCondition(this::hasMatchingDate);
+        DateCheckStrategy dateCheckStrategy = this::hasMatchingDate;
+        return decemberEventPlan.isDateSatisfyingDateCondition(dateCheckStrategy);
+    }
+
+    @Override
+    public boolean isSatisfyingCondition(LocalDate date) {
+        return hasMatchingDate(date);
     }
 }
 
